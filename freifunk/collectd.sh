3328959ff28333346ec180dd06ba1c2e36a80082
Markus Blaschke <mblaschke82@gmail.com>
2017-01-22 13:40:01 +0100

    Introduced configuration Add workaround for collectd numeric mac addresses

--- freifunk/collectd.sh
+++ freifunk/collectd.sh
@@ -11,17 +11,31 @@ set -o errtrace  ## trace ERR through 'time command' and other functions
 set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
 set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
 
+# Automatic update
+# -> 1 = enabled
+# -> 0 = disabled
+CONF_AUTO_UPDATE=1
+
+# -> 0 = always
+# -> 5 = every 5 minutes
+CONF_AUTO_UPDATE_AGE=5
+
+# URL for nodes.json
+CONF_NODE_JSON_URL="http://hg.albi.info/json/nodes.json"
+
+# Storage path for nodes.json
+CONF_NODE_JSON_PATH="/dev/shm/ffs-stats-nodes.json"
+
+
+### do not change below ###
+
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
-NODES_JSON="/dev/shm/ffs-stats-nodes.json"
-NODES_URL="http://hg.albi.info/json/nodes.json"
 HOSTNAME="$1"
 INTERVAL="${COLLECTD_INTERVAL:-60}"
-AUTO_UPDATE=1
-UPDATE_AGE=5
 
 update_nodes_json() {
 	RUN_UPDATE=0
-	if [[ ! -f "${NODES_JSON}" ]]; then
+	if [[ ! -f "${CONF_NODE_JSON_PATH}" ]]; then
 		RUN_UPDATE=1
 	fi
 
@@ -29,7 +43,7 @@ update_nodes_json() {
 		RUN_UPDATE=1
 	fi
 
-	if test $(find "${NODES_JSON}" -mmin +"$UPDATE_AGE" &> /dev/null); then
+	if test $(find "${CONF_NODE_JSON_PATH}" -mmin +"$CONF_AUTO_UPDATE_AGE" &> /dev/null); then
 		RUN_UPDATE=1
 	fi
 
@@ -37,12 +51,12 @@ update_nodes_json() {
 		# check age
 		if [[ "$RUN_UPDATE" -eq 1 ]]; then
 			UPDATE_TIME=$(date +%s)
-			wget -O"${NODES_JSON}.tmp" "$NODES_URL" &> /dev/null
-			if [[ -f "${NODES_JSON}.tmp" ]]; then
-				mv -f "${NODES_JSON}.tmp" "${NODES_JSON}" &> /dev/null || rm -f "${NODES_JSON}.tmp"
-				rm -f "${NODES_JSON}.tmp"
+			wget -O"${CONF_NODE_JSON_PATH}.tmp" "$CONF_NODE_JSON_URL" &> /dev/null
+			if [[ -f "${CONF_NODE_JSON_PATH}.tmp" ]]; then
+				mv -f "${CONF_NODE_JSON_PATH}.tmp" "${CONF_NODE_JSON_PATH}" &> /dev/null || rm -f "${CONF_NODE_JSON_PATH}.tmp"
+				rm -f "${CONF_NODE_JSON_PATH}.tmp"
 				UPDATE_TIME=$(($(date +%s)-UPDATE_TIME))
-				UPDATE_SIZE=$(stat -c%s "${NODES_JSON}")
+				UPDATE_SIZE=$(stat -c%s "${CONF_NODE_JSON_PATH}")
 				echo "PUTVAL \"freifunk/stats/gauge-update_time\" interval=${INTERVAL} N:$UPDATE_TIME"
 				echo "PUTVAL \"freifunk/stats/gauge-update_filesize\" interval=${INTERVAL} N:$UPDATE_SIZE"
 			fi
@@ -104,7 +118,7 @@ to_float() {
 
 main() {
 	UPDATE_FINSIHED=0
-	if [[ "$AUTO_UPDATE" -eq 1 ]]; then
+	if [[ "$CONF_AUTO_UPDATE" -eq 1 ]]; then
 		update_nodes_json
 		sleep 1
 	fi
@@ -117,8 +131,12 @@ main() {
 				;;
 
 			*)
-				if [[ -f "${NODES_JSON}" ]]; then
-					NODE_JSON=$(cat "${NODES_JSON}" | jq -c ".nodes[] | select(.nodeinfo.node_id == \"$NODE_ID\")")
+				# fix numeric translation from collectd 
+				# workaround if mac-address is numeric and not quoted inside collect.dconf
+				NODE_ID=$(echo "$NODE_ID" | cut -d '.' -f 1)
+
+				if [[ -f "${CONF_NODE_JSON_PATH}" ]]; then
+					NODE_JSON=$(cat "${CONF_NODE_JSON_PATH}" | jq -c ".nodes[] | select(.nodeinfo.node_id == \"$NODE_ID\")")
 					NODE_STATS_JSON=$(echo "${NODE_JSON}" | jq -c ".statistics")
 					NODE_NAME=$(echo "${NODE_JSON}" | jq -r ".nodeinfo.hostname")
 
