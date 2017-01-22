2af3725144046b8fa4e83fde51f76a95e82185a3
Markus Blaschke <mblaschke82@gmail.com>
2017-01-22 12:28:41 +0100

    Improve ffs handling

--- freifunk/collectd.sh
+++ freifunk/collectd.sh
@@ -51,59 +51,97 @@ update_nodes_json() {
 	fi
 }
 
+collectd_translate_name() {
+  key="$*"
+  key=$(echo "$key" | sed 's/[^[:print:]]//')
+  key=${key//./_}
+  key=${key//\//_}
+  key=${key// /_}
+  echo "$key"
+}
+
+collectd_translate_variable() {
+  key="$*"
+  key=$(echo "$key" | sed 's/[^[:print:]]//')
+  key=${key//./_}
+  key=${key//\//_}
+  key=${key// /_}
+  echo "$key"
+}
+
 extract_stat() {
-	VAR_NAME=$1
-	COLLECTD_NODE=$2
-	VAR_VALUE=$(echo "$NODE_STATS_JSON" | jq ".$VAR_NAME")
+	NODE_ID=$(collectd_translate_name "$NODE_ID")
+	NODE_NAME=$(collectd_translate_name "$NODE_NAME")
+	VAR_NAME="$1"
+	COLLECTD_NODE=$(collectd_translate_variable "$2")
+	VAR_VALUE=$(echo "$NODE_STATS_JSON" | jq -r ".$VAR_NAME")
 
 	if [[ "$#" -eq 3 ]]; then
-		VAR_VALUE=$($3 "$VAR_VALUE")
+		VAR_VALUE=$("$3" "$VAR_VALUE")
 	fi
-	echo "PUTVAL \"freifunk/node-${NODE_ID}/${COLLECTD_NODE}\" interval=${INTERVAL} N:${VAR_VALUE}"
+
+	echo "PUTVAL \"freifunk/node-${NODE_ID}-${NODE_NAME}/${COLLECTD_NODE}\" interval=${INTERVAL} N:${VAR_VALUE}"
 }
 
 const_stat() {
-	COLLECTD_NODE=$1
-	VAR_VALUE=$2
-	echo "PUTVAL \"freifunk/node-${NODE_ID}/${COLLECTD_NODE}\" interval=${INTERVAL} N:${VAR_VALUE}"
+	NODE_ID=$(collectd_translate_name "$NODE_ID")
+	NODE_NAME=$(collectd_translate_name "$NODE_NAME")
+	COLLECTD_NODE=$(collectd_translate_variable "$1")
+	VAR_VALUE="$2"
+
+	echo "PUTVAL \"freifunk/node-${NODE_ID}-${NODE_NAME}/${COLLECTD_NODE}\" interval=${INTERVAL} N:${VAR_VALUE}"
 }
 
 to_int() {
-	printf '%.*f' 0 "$1"
+        VAL=$(echo "$1" | sed 's/[^[:print:]]//')
+	printf '%.*f' 0 "$VAL"
 }
 
 to_float() {
-	printf '%.*f' 5 "$1"
+        VAL=$(echo "$1" | sed 's/[^[:print:]]//')
+	printf '%.*f' 5 "$VAL"
 }
 
-UPDATE_FINSIHED=0
-if [[ "$AUTO_UPDATE" -eq 1 ]]; then
-	update_nodes_json
-fi
+main() {
+	UPDATE_FINSIHED=0
+	if [[ "$AUTO_UPDATE" -eq 1 ]]; then
+		update_nodes_json
+		sleep 1
+	fi
 
-COLLECT_TIME=$(date +%s)
-for NODE_ID in "$@"; do
-	case "$NODE_ID" in
-		"update")
-			update_nodes_json
-			;;
+	COLLECT_TIME=$(date +%s)
+	for NODE_ID in $@; do
+		case "$NODE_ID" in
+			"update")
+				update_nodes_json
+				;;
 
-		*)
-			if [[ -f "${NODES_JSON}" ]]; then
-				NODE_STATS_JSON=$(cat "${NODES_JSON}" | jq -c ".nodes[] | select(.nodeinfo.node_id == \"$NODE_ID\") | .statistics")
-				if [[ "$NODE_STATS_JSON" != '{}' ]]; then
-					const_stat "power" 1
-					extract_stat "uptime" "uptime" "to_int"
-					extract_stat "loadavg" "gauge-loadavg" "to_float"
-					extract_stat "clients" "users" "to_int"
-					extract_stat "memory_usage" "percent-memory" "to_float"
-					extract_stat "rootfs_usage" "percent-rootfs" "to_float"
-				else
-					const_stat "power" 0
+			*)
+				if [[ -f "${NODES_JSON}" ]]; then
+					NODE_JSON=$(cat "${NODES_JSON}" | jq -c ".nodes[] | select(.nodeinfo.node_id == \"$NODE_ID\")")
+					NODE_STATS_JSON=$(echo "${NODE_JSON}" | jq -c ".statistics")
+					NODE_NAME=$(echo "${NODE_JSON}" | jq -r ".nodeinfo.hostname")
+
+					if [[ "$NODE_STATS_JSON" != '{}' ]]; then
+						const_stat "power" 1
+						extract_stat "uptime" "uptime" "to_int"
+						extract_stat "loadavg" "gauge-loadavg" "to_float"
+						extract_stat "clients" "users" "to_int"
+						extract_stat "memory_usage" "percent-memory" "to_float"
+						extract_stat "rootfs_usage" "percent-rootfs" "to_float"
+					fi
 				fi
-			fi
-			;;
-	esac
+				;;
+		esac
+	done
+	COLLECT_TIME=$(($(date +%s)-COLLECT_TIME))
+	echo "PUTVAL \"freifunk/stats/gauge-collect_time\" interval=${INTERVAL} N:$COLLECT_TIME"
+}
+
+# first run
+main $@
+
+# loop'ed run
+while sleep "$INTERVAL"
+	main $@
 done
-COLLECT_TIME=$(($(date +%s)-COLLECT_TIME))
-echo "PUTVAL \"freifunk/stats/gauge-collect_time\" interval=${INTERVAL} N:$COLLECT_TIME"
